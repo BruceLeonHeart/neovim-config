2fcca38fb59aacef68d56c1adb9e9591de1dc899
Bruce <fkcode@DESKTOP-UJ3P9BO>
2024-12-08 21:50:51 +0800

    [ADD] add clang_format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,15 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+local opt = vim.opt
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.api.nvim_create_autocmd({"FileType"}, {
+    pattern = {"c", "cpp", "md", "txt", "c.snippets", "cpp.snippets","py"},
+    callback = function()
+        vim.b.autoformat = true
+        vim.opt.tabstop = 4
+        vim.opt.shiftwidth = 4
+        vim.opt.softtabstop = 4
+    end,
+})
+
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,20 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-  opts = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-      },
-    })
-  end,
+	"nvimtools/none-ls.nvim",
+	opts = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.clang_format.with({
+					extra_args = {
+						"-style={BasedOnStyle: LLVM, AllowShortFunctionsOnASingleLine: None ,IndentWidth: 4}",
+					},
+				}),
+			},
+		})
+	end,
 
-  keys = {
-    {
-      "<leader>gf",
-      vim.lsp.buf.format,
-      { "n", "v" },
-      {},
-    },
-  },
+	keys = {
+		{
+			"<leader>gf",
+			vim.lsp.buf.format,
+			{ "n", "v" },
+			{},
+		},
+	},
 }
